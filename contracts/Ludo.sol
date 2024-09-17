ce64213482843ea89e02e4ceeb83ba8bc95392ef
shegspear <shegspear@gmail.com>
2024-09-17 17:59:31 +0100

    started working on Ludo contract

---
+++ contracts/Ludo.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+contract Ludo {
+    address public owner;
+
+    uint8 public steps;
+    uint256 public dice;
+
+    constructor() {
+       owner = msg.sender; 
+    }
+
+    function rollDice() public {
+        uint256 randomHash = uint256(
+            keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender))
+        );
+        
+        dice = (randomHash % 6) + 1;
+    }
+
+
+
+}
